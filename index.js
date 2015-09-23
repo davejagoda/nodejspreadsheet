ea52904d269d5df834eb5b118b3af34f47a8bcc1
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-09-23 16:41:21 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var Spreadsheet = require('edit-google-spreadsheet');
+ 
+Spreadsheet.load({
+  debug: true,
+  spreadsheetName: process.env.SPREADSHEET,
+  worksheetName: process.env.WORKSHEET,
+  oauth : {
+    email: process.env.EMAIL,
+    keyFile: process.env.KEYFILE
+  },
+}, function sheetReady(err, spreadsheet) {
+console.log(process.env.EMAIL);
+console.log(process.env.KEYFILE);
+console.log(process.env.SPREADSHEET);
+console.log(process.env.WORKSHEET);
+});
